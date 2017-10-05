acc4e460d1f3982007be634fc0e8045d8ae81df3
xuqi93 <976128151@qq.com>
2017-10-05 16:15:22 +0800

    test

---
+++ src/main/java/com/xuqi/gril/GrilApplication.java
@@ -0,0 +1,12 @@
+package com.xuqi.gril;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrilApplication.class, args);
+	}
+}
